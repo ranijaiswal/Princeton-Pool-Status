d21a080f0a3283cae5d1be3ac60031a3ba9669a8
Rani Jaiswal <ruj96@live.com>
2017-03-28 03:11:33 -0400

    status site commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>PrincetonPool Status</title>
+</head>
+<body>
+	<h1> PrincetonPool </h1>
+	<h3> Project Manager: Jonathan Balkind </h3>
+	<h3> Project Leader: Annie Chen </h3>
+	<h3> Team: </h3>
+	<ul>
+	<li> Annie Chen - anniec@princeton.edu </li>
+	<li> Rani Jaiswal - rjaiswal@princeton.edu  </li>
+	<li> Cindy Liu - cindyliu@princeton.edu </li>
+	<li> Alice Tang - xt6@princeton.edu </li>
+	</ul>
+	<h3> <a href="https://docs.google.com/document/d/19IrN09ag0GRdlq_4Jyv789RsXCkZxkTT3WL-biAkqLU/edit?usp=sharing">Design Document</a></h3>
+	<h3>Timeline</h3>
+	<img src="timeline.png" alt="Timeline" style="width:579px;height:565px;"> 
+	<h3> Elevator Pitch </h3>
+	<h3> Demo Slides </h3>
+	<h3> Final Document </h3>
+</body>
+</html>
