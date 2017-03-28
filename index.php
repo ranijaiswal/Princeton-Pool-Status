1c02b79706f4662816284fd1b10b3f2434cc7e80
Annie Chen <chen.a.edna@gmail.com>
2017-03-28 13:46:32 -0400

    updated timeline to html table

--- index.php
+++ index.php
@@ -15,8 +15,97 @@
 	<li> Alice Tang - xt6@princeton.edu </li>
 	</ul>
 	<h3> <a href="https://docs.google.com/document/d/19IrN09ag0GRdlq_4Jyv789RsXCkZxkTT3WL-biAkqLU/edit?usp=sharing">Design Document</a></h3>
+	
+	<!--Timeline Table-->
 	<h3>Timeline</h3>
-	<img src="timeline.png" alt="Timeline" style="width:579px;height:565px;"> 
+	
+	<style>
+		tr {text-align:center;}
+	  #table-head {
+	    background-color: #ccc;
+	    height: 50px;
+	  }
+	  #milestone {
+	    text-align:left;
+	  }
+	  #even {background-color: #eee;}
+	</style>
+
+	<div id = "table-tall">
+	<table style="width:100%">
+	  
+	<!--col widths-->
+	  <col>
+	  <col width="100">
+	  <col width="200">
+	  <col>
+
+	<!---->
+
+	  <tr id="table-head">
+	    <th>Week</th>
+	    <th style = "width:50">Dates</th> 
+	    <th>Deadlines</th>
+	    <th>Milestone(s)</th>
+	  </tr>
+	  <tr>
+	    <td>1</td>
+	    <td>3/19 - 3/25</td>
+	    <td>Spring Break</td>
+	    <td>Tutorials, division of labor, set up GitHub repository, get familiar with GitHub</td>
+	  </tr>
+	  <tr id="even">
+	    <td>2</td>
+	    <td>3/26 - 4/1</td>
+	    <td>Assignment 5 due 3/31</td>
+	    <td>First TA meeting, continue with tutorials, decide specialization</td>
+	  </tr>
+	  <tr>
+	    <td>3</td>
+	    <td>4/2 - 4/8</td>
+	    <td>-</td>
+	    <td>Very basic functionality for each component (i.e. backend server is running, data can be fetched from database and rendered on webpage)</td>
+	    
+	  </tr>
+	  <tr id="even">
+	    <td>4</td>
+	    <td>4/9 - 4/15</td>
+	    <td>Project Prototype 4/16</td>
+	    <td>Working frontend (i.e. buttons and drop-down lists, basic layout of page), ability to make a request and have it show up on the page</td>
+	  </tr>
+	  <tr>
+	    <td>5</td>
+	    <td>4/16 - 4/22</td>
+	    <td>-</td>
+	    <td>CAS login implementation</td>
+	  </tr>
+	  <tr id="even">
+	    <td>6</td>
+	    <td>4/23 - 4/29</td>
+	    <td>Alpha Test 4/28</td>
+	    <td>Implement notifications by email (and text); should be able to determine a preliminary measure of project success</td>
+	  </tr>
+	  <tr>
+	    <td>7</td>
+	    <td>4/30 - 5/6</td>
+	    <td>Beta Test 5/5</td>
+	    <td>Have independents (users) try it and give feedback, fix corner cases</td>
+	  </tr>
+	  <tr id="even">
+	    <td>8</td>
+	    <td>5/7 - 5/13</td>
+	    <td>Demo Days 5/8 - 5/10</td>
+	    <td>Fix more corner cases that arise from demo day</td>
+	  </tr>
+	  <tr>
+	    <td>9</td>
+	    <td>5/14</td>
+	    <td>Final Project Due Midnight</td>
+	    <td>Submit</td>
+	  </tr>
+	</table>
+	</div>
+
 	<h3> Elevator Pitch </h3>
 	<h3> Demo Slides </h3>
 	<h3> Final Document </h3>
